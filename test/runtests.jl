2e031b38dff01946ce7cf79bbbaa97aea3a762c9
Lyndon White <oxinabox@ucc.asn.au>
2018-08-11 16:35:10 +0100

    abbility to do address checks more generally

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,9 @@ using InternedStrings
 
 using Test, Random, Base.GC
 
-addr_eq(a,b) = pointer(a) === pointer(b)
+addr_eq(a::String, b::String) = pointer(a) === pointer(b)
+addr_eq(a, b) = objectid(a) === objectid(b)
+
 
 @testset "All kinds of types" begin include("all_kinds_of_types.jl") end
 @testset "String macro"       begin include("string_macro.jl") end
